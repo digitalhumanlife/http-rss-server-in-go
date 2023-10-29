175972385e01c5caa43e5285ea0422f47edeef7d
RockChun <rockchun@RockChunui-MacBookPro.local>
2023-10-29 23:54:41 +0900

    boilerplate for http RSS server

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in ENV.")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Lnik"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1router := chi.NewRouter()
+	v1router.Get("/readiness", handlerReadiness)
+	v1router.Get("/err", handlerError)
+
+	router.Mount("/v1", v1router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+	log.Printf("Server is running on port %s", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("PORT is ", portString)
+}
